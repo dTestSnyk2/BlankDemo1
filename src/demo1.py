237ccd1316b567d6b6a5e204c6db5ab409f43c43
dTestSnyk2 <dTestSnyk2@dplatform.com>
2022-08-26 12:28:41 +1000

    testing snyk

--- src/demo1.py
+++ src/demo1.py
@@ -2,20 +2,21 @@
 print('some innocuous code output')
 print('some innocuous code output')
 print('some innocuous code output')
-###import re
-###import string
-###
-###import commands
-###template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
-###
-###
-####Typing in an expression like 2 * 3 will result in Result = 6 , which is the desired outcome. A malicious user, on the other hand, could type in something like __import__(‘os’).system(‘rm –rf /’)  as input. And this results in a deletion of all files and directories in the script’s folder if the process has enough privileges. 
-###
-###compute = input('\nYour calculation? => ')
-###if not compute:
-###    print ("No input")
-###else:
-###    print ("Result =", eval(compute))
+
+import re
+import string
+
+import commands
+template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
+
+
+#Typing in an expression like 2 * 3 will result in Result = 6 , which is the desired outcome. A malicious user, on the other hand, could type in something like __import__(‘os’).system(‘rm –rf /’)  as input. And this results in a deletion of all files and directories in the script’s folder if the process has enough privileges. 
+
+compute = input('\nYour calculation? => ')
+if not compute:
+    print ("No input")
+else:
+    print ("Result =", eval(compute))
    
 #To prevent such a thing from happening, we need to validate the expressions input by a user. Something like this: 
 #def validate(untrusted_input):
